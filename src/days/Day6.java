73be309fa6f306246014ce40acd629ece74f979f
Joe Moore <joverandout@gmail.com>
2023-02-11 21:29:24 +0000

    day 6 part 2

--- src/days/Day6.java
+++ src/days/Day6.java
@@ -2,38 +2,46 @@ package days;
 
 import common.Day;
 
+import java.util.HashSet;
+
 public class Day6 extends Day {
 
     @Override
     public Object[] getSolutions(String fileAsText) {
-        char[] currentFourChars = new char[4];
         char[] inputAsArray = fileAsText.toCharArray();
 
-        int partOne = getPosition(currentFourChars, inputAsArray);
+        int partOne = getPosition(inputAsArray, 4);
+        int partTwo = getPosition(inputAsArray, 14);
 
         Object[] solutions = new Object[2];
         solutions[0] = partOne;
-        solutions[1] = partOne;
+        solutions[1] = partTwo;
         return solutions;
     }
 
     private boolean allFourCharsDiffer(char[] curr){
-        return ((curr[0] != curr[1] && curr[0] != curr[2] && curr[0] != curr[3])
-                && (curr[1] != curr[2] && curr[1] != curr[3]) && (curr[2] != curr[3]));
+        var valueList = new HashSet<Character>();
+        for (var v : curr)
+        {
+           if (valueList.contains(v)) return false;
+           else valueList.add(v);
+        }
+        return true;
     }
 
-    private int getPosition(char[] currentFourChars, char[] inputAsArray){
+    private int getPosition(char[] inputAsArray, int lengthVal){
+        char[] currentFourChars = new char[lengthVal];
         int pastRemove = 0;
         for (int i = 0; i < inputAsArray.length; i++) {
-            if (i < 4){
+            if (i < lengthVal){
                 currentFourChars[i] = inputAsArray[i];
-                if(i == 4 && allFourCharsDiffer(currentFourChars)){
+                if(i == lengthVal && allFourCharsDiffer(currentFourChars)){
                     return i+1;
                 }
             }
             else {
                 currentFourChars[pastRemove] = inputAsArray[i];
-                if(pastRemove < 3) pastRemove++;
+                if(pastRemove < lengthVal-1) pastRemove++;
                 else pastRemove = 0;
                 if(allFourCharsDiffer(currentFourChars)){
                     return i+1;
@@ -42,5 +50,4 @@ public class Day6 extends Day {
         }
         return inputAsArray.length;
     }
-
 }
