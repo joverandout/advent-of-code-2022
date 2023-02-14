76c49a7389d4b54f03adf18c59b0f5d72eab6214
Joe Moore <joverandout@gmail.com>
2023-02-14 21:48:33 +0000

    Day 7 KVP class and initial scanner work

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        for (int day = 1; day <= 6; day++) {
+        for (int day = 1; day <= 7; day++) {
             System.out.println("==Day " + day + "==");
             Day instance = (Day) Class.forName("days.Day" + day).getDeclaredConstructor().newInstance();
             instance.PrintParts(readFile(day));
--- src/days/Day7.java
+++ src/days/Day7.java
@@ -1,12 +1,41 @@
 package days;
 
 import common.Day;
+import common.util.KeyValue;
+import common.util.Node;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Day7 extends Day {
 
     @Override
     public Object[] getSolutions(String fileAsText) {
         char[] inputAsArray = fileAsText.toCharArray();
+
+        KeyValue<String, Integer> root = new KeyValue<>("/", 0);
+        Node<KeyValue> rootNode = new Node<KeyValue>(root);
+
+        Node currentNode = rootNode;
+
+        try{
+            Scanner scanner = new Scanner(fileAsText);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                Scanner lineScanner = new Scanner(line);
+                boolean cd = false, listAll = false;
+                while(lineScanner.hasNext()) {
+                    String token = lineScanner.next();
+                }
+            }
+
+            return new Object[]{2, 3};
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new Object[]{22, 3};
     }
 
 }
