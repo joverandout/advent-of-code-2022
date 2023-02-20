5fcb7642524edfe810c5758ae2fc997c1e48a142
Joe Moore <joverandout@gmail.com>
2023-02-20 22:16:14 +0000

    Day 8 part 2 start

--- src/days/Day8.java
+++ src/days/Day8.java
@@ -24,11 +24,11 @@ public class Day8 extends Day {
 
         Object[] solutions = new Object[2];
         solutions[0] = partOne(forest);
-        solutions[1] = 2;
+        solutions[1] = partTwo(forest);
         return solutions;
     }
 
-    private int partOne(char[][] forest){
+    private int partOne(char[][] forest) {
         int count = 0;
         for (int i = 0; i < forest.length; i++) {
             for (int j = 0; j < forest[i].length; j++) {
@@ -45,7 +45,20 @@ public class Day8 extends Day {
         return count;
     }
 
-    private boolean checkHorizontal(char[][] forest, int i, int j){
+
+    private int partTwo(char[][] forest) {
+        int max = 0;
+        for (int i = 0; i < forest.length; i++) {
+            for (int j = 0; j < forest[i].length; j++) {
+                if(getScenicScoreVertical(forest, i, j) * getScenicScoreHorizontal(forest, i, j) > max){
+                    max = getScenicScoreVertical(forest, i, j) * getScenicScoreHorizontal(forest, i, j);
+                }
+            }
+        }
+        return max;
+    }
+
+    private boolean checkHorizontal(char[][] forest, int i, int j) {
         boolean leftOfIt = true, rightOfIt = true;
         for (int k = j+1; k < forest[i].length; k++) {
             if(forest[i][j] <= forest[i][k]){
@@ -62,7 +75,7 @@ public class Day8 extends Day {
         return leftOfIt || rightOfIt;
     }
 
-    private boolean checkVertical(char[][] forest, int i, int j){
+    private boolean checkVertical(char[][] forest, int i, int j) {
         boolean upOftIt = true, downOfIt = true;
         for (int k = i+1; k < forest.length; k++) {
             if(forest[i][j] <= forest[k][j]){
@@ -77,6 +90,41 @@ public class Day8 extends Day {
             }
         }
         return upOftIt || downOfIt;
+    }
+
+
+    private int getScenicScoreVertical(char[][] forest, int i, int j) {
+        int upOftIt = 0, downOfIt = 0;
+        for (int k = i+1; k < forest.length; k++) {
+            downOfIt++;
+            if(forest[i][j] <= forest[k][j]){
+                break;
+            }
+        }
+        for (int k = 0; k < i; k++) {
+            upOftIt++;
+            if(forest[i][j] <= forest[k][j]){
+                break;
+            }
+        }
+        return downOfIt*upOftIt;
+    }
+
+    private int getScenicScoreHorizontal(char[][] forest, int i, int j) {
+        int rightOfIt = 0, leftOfIt = 0;
+        for (int k = j+1; k < forest[i].length; k++) {
+            rightOfIt++;
+            if(forest[i][j] <= forest[i][k]){
+                break;
+            }
+        }
+        for (int k = 0; k < j; k++) {
+            leftOfIt++;
+            if(forest[i][j] <= forest[i][k]){
+                break;
+            }
+        }
+        return leftOfIt*rightOfIt;
     }
 
 }
